01f9d6aea5b696627410bf8f9087c7e42d986285
Ali <abdulmuqsit10101@gmail.com>
2020-06-14 19:30:50 +0500

    Dealing with static files using express.static() in app.use

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')();
-const app = express;
+const express = require('express');
+const app = express();
 const port = 3000;
+
+app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/users', (req, res) => {
-  res.send('We are Users');
+app.get('/index', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => console.log(`App is running on ${port}`));
