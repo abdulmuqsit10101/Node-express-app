9c5a62bb16511d5b3890cba760b1323adf66e863
Ali <abdulmuqsit10101@gmail.com>
2020-06-14 20:00:13 +0500

    Dynamic Path or use of Params.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+route = require('express').Router({strict: true});
+app.use('/strict/', route);
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
@@ -10,6 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/index', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/user/:userId/book/:bookId', (req, res) => {
+  const dataScript = 'User id is ' + req.params.userId + ' with book '
+    + req.params.bookId;
+  res.send(dataScript);
 });
 
 app.listen(port, () => console.log(`App is running on ${port}`));
